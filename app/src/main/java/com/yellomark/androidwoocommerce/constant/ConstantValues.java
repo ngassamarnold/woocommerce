649a63736a27a0de20d459851018e2084974da38
johnsyntaxe <djonathanzete@gmail.com>
2019-02-20 17:12:08 +0100

    Ajoute de la modification de yello mark version 1

---
+++ app/src/main/java/com/yellomark/androidwoocommerce/constant/ConstantValues.java
@@ -0,0 +1,58 @@
+package com.yellomark.androidwoocommerce.constant;
+
+
+/**
+ * ConstantValues contains some constant variables, used all over the App.
+ **/
+
+
+public class ConstantValues {
+    
+    //*********** API Base URL ********//
+    public static final String WOOCOMMERCE_URL =  "http://stillforceltd.com/market-place/";
+    
+    public static final String WOOCOMMERCE_CONSUMER_KEY = "ck_d051851e033d7323ee0ae938ebfe99fd210dc450";
+    public static final String WOOCOMMERCE_CONSUMER_SECRET = "cs_e820bf05a0c37d3e721cad5d127fb91a25713d42";
+    
+    
+    public static String CODE_VERSION = "1.5";
+    
+    
+    public static final boolean IS_CLIENT_ACTIVE = true;                               // "false" if compiling the project for Demo, "true" otherwise
+    
+    public static String APP_HEADER;
+    
+    public static String DEFAULT_HOME_STYLE;
+    public static String DEFAULT_CATEGORY_STYLE;
+    
+    public static String LANGUAGE_ID;
+    public static String LANGUAGE_CODE;
+    public static String CURRENCY_SYMBOL;
+    public static String FILTER_MAX_PRICE;
+    public static long NEW_PRODUCT_DURATION;
+    
+    public static boolean IS_GUEST_CHECKOUT_ENABLED;
+    public static boolean IS_ONE_PAGE_CHECKOUT_ENABLED;
+    
+    public static boolean IS_GOOGLE_LOGIN_ENABLED=true;
+    public static boolean IS_FACEBOOK_LOGIN_ENABLED=true;
+    public static boolean IS_ADD_TO_CART_BUTTON_ENABLED;
+    
+    public static boolean IS_ADMOBE_ENABLED;
+    public static String ADMOBE_ID="ca-app-pub-4302071774857897~9618655719";
+    public static String AD_UNIT_ID_BANNER;
+    public static String AD_UNIT_ID_INTERSTITIAL;
+    
+    public static String ABOUT_US;
+    public static String TERMS_SERVICES;
+    public static String PRIVACY_POLICY;
+    public static String REFUND_POLICY;
+    
+    public static boolean IS_USER_LOGGED_IN;
+    public static boolean IS_GUEST_LOGGED_IN = false;
+    public static boolean IS_PUSH_NOTIFICATIONS_ENABLED;
+    public static boolean IS_LOCAL_NOTIFICATIONS_ENABLED;
+    
+    public static String SELLER_ID;
+    
+}
